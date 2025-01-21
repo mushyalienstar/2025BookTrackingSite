801fc6740ba9de7bce04588e2f1b2e6455cc1c26
mushyalienstar <160424969+mushyalienstar@users.noreply.github.com>
2025-01-21 14:17:58 -0500

    Your commit message here

--- script.js
+++ script.js
@@ -7,11 +7,31 @@ const booksRead = [
         pages: 544,
     },
     { 
-        title: "The Poppy War", 
+        title: "The Dragon Republic", 
         author: "R.F. Kuang", 
-        cover: "https://ebooks.gramedia.com/ebook-covers/49807/image_highres/ID_POP2019MTH10EPO.jpg", 
-        pages: 544,
-    }
+        cover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1604740022i/55854821.jpg", 
+        pages: 675,
+    },
+    /*
+    { 
+        title: "The Burning God", 
+        author: "R.F. Kuang", 
+        cover: "https://gpu.id/data-gpu/images/img-book/93984/623188001.jpg", 
+        pages: 640,
+    },
+    { 
+        title: "The Chalice of the Gods", 
+        author: "Rick Riordan", 
+        cover: "https://dynamic.indigoimages.ca/v1/books/books/1368098177/1.jpg?width=810&maxHeight=810&quality=85", 
+        pages: 288,
+    },
+    { 
+        title: "Wrath of the Triple Goddess", 
+        author: "Rick Riordan", 
+        cover: "https://m.media-amazon.com/images/I/91FkwrxEvCL.jpg", 
+        pages: 336,
+    },
+    */
 ];
 
 const totalBooks = 20;
